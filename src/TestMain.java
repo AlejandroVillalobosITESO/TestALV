e5ed76970097a159db8629a6c046d37eae7985de
Alejandro Villalobos <is728135@iteso.mx>
2021-02-17 17:52:49 -0600

    Test 4
    Esto es algo bueno, vamos a meterlo a main

--- src/TestMain.java
+++ src/TestMain.java
@@ -2,5 +2,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("Test2");
+        System.out.println("Ahora esto es algo bueno");
     }
 }
