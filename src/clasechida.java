42323b1e25323eb71dfcadffaec9db2b8578bb67
Alejandro Villalobos <is728135@iteso.mx>
2021-02-17 18:22:33 -0600

    Le puse chingaderas a la clase chida

--- src/clasechida.java
+++ src/clasechida.java
@@ -1,3 +1,26 @@
 public class clasechida {
     //this will do stuff
+    private int a;
+    private int b;
+
+    public clasechida(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 }
