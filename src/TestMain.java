625824fa77e55cdb425fb3a2788444cf190500fe
Alejandro Villalobos <is728135@iteso.mx>
2021-02-17 18:25:58 -0600

    Le agregue cosas a TestMain

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,5 +3,7 @@ public class TestMain {
         System.out.println("Test1");
         System.out.println("Test2");
         System.out.println("Ahora esto es algo bueno");
+        clasechida objeto1 = new clasechida(10,20);
+        System.out.println("A = "+objeto1.getA()+"\nB = "+objeto1.getB());
     }
 }
